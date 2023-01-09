a208d2387e0c86b34faaf1076c10ce59547246a8
ShubhamJoshii <shubhamjoshii676@gmail.com>
2023-01-09 17:52:13 +0530

    React Routing Fix With Express

--- index.js
+++ index.js
@@ -8,15 +8,18 @@ const app = express();
 app.use(cookieParser())
 app.use(express.json());
 
-
 const User = require("./Database")
 app.use(require("./auth"))
 
+app.use(express.static(path.resolve(__dirname,"client","build")));
 app.get("/",(req,res)=>{
     console.log(path.resolve(__dirname,"client","build"))
-    app.use(express.static(path.resolve(__dirname,"client","build")));
     res.status(200).sendFile(path.resolve(__dirname,"client","build"));  
 })
+
+app.get('*', (req,res) =>{
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
 
 app.listen("8000",()=>{
     console.log("Server Connected at 8000")
